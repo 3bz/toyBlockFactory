848b1b1059c2c5eb7f5a34301dd9fbbc9612d8d0
Ryan Ebsworth <ebzyboy64@hotmail.com>
2019-10-14 10:26:35 +1100

    template invoice created, generating with fixed length to display nicely.

--- src/main/java/Accountant.java
+++ src/main/java/Accountant.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Accountant {
 
     public Accountant(){}
 
-    public int calculateOrder(List<Blueprint> customerOrder) {
+    public void analyseOrderForCalculating(CustomerOrder customerOrder) {
+        List<Blueprint> expenses = new ArrayList<>();
+        for (Blueprint itemOrder : customerOrder.getSpecification())
+            expenses.add(itemOrder);
+        int totalCost = calculateOrder(expenses);
+        //processInvoice(customerOrder);
+    }
+
+    public int calculateOrder(List<Blueprint> expenses) {
         int result = 0;
-        for (Blueprint cost : customerOrder)
+        for (Blueprint cost : expenses)
             result += calculateCost(cost);
 
         return result;
@@ -16,7 +25,7 @@ public class Accountant {
         int result = 0;
         result += order.getShapePlanned().getPrice();
         if (order.getColorPlanned().equals(Color.RED))
-            result++;
+            result+= Color.RED.getPrice();
 
         result *= order.getQuantityOfBlock();
         return result;
